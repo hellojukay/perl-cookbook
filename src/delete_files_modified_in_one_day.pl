b7f103d193096e41e502bbba71955ca783df8094
licong <licong@qianxin.com>
2020-07-28 18:41:04 +0800

    添加注释

--- src/delete_files_modified_in_one_day.pl
+++ src/delete_files_modified_in_one_day.pl
@@ -1,6 +1,7 @@
 #!/bin/perl
 my @files = glob "/home/hellojukay/Desktop/*";
 foreach $file (@files) {
+    # -M 返回文件修改距离现在的时间距离，单位是：天
     my $modify_date = -M $file;
     if ($modify_date < 1.0) {
         next if ($file =~ /delete/);
