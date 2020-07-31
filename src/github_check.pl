4c8fe5307dbb1eb0640961f4735c7ad3e2ab4d52
licong <licong@qianxin.com>
2020-07-31 22:07:46 +0800

    fix github check

--- src/github_check.pl
+++ src/github_check.pl
@@ -8,21 +8,59 @@ use strict;
 
 use HTTP::Tiny;
 use JSON;
+use Time::HiRes qw(gettimeofday);
 
 # 返回 github 的所有 ip 地址
-sub github_ips {
-    my $response = HTTP::Tiny->new->get('https://api.github.com/meta',{headers => {'Accept' => 'plication/vnd.github.v3+json"'}});
-    if($response->{status} != 200){
+sub request_github {
+    my $response = HTTP::Tiny->new->get(
+        'https://api.github.com/meta',
+        {
+            headers => { 'Accept' => 'plication/vnd.github.v3+json"' }
+        }
+    );
+    if ( $response->{status} != 200 ) {
         die "github response code  $response->{status}";
     }
 
-    my $content = $response->{content};
-    my $json = JSON->new->allow_nonref;
+    my $content      = $response->{content};
+    my $json         = JSON->new->allow_nonref;
     my $response_obj = $json->decode($content);
-    return @{$response_obj->{git}};
+
+    my @ips = @{ $response_obj->{git} };
+    push @ips, @{ $response_obj->{web} };
+
+    my @result;
+    foreach my $ip (@ips) {
+
+        # 原 ip 格式为 "192.30.252.0/22",
+        my ( $ip, $mask ) = split( /\//, $ip );
+        push @result, $ip;
+    }
+    return @result;
 }
-my @ips = github_ips();
+
+sub check_http {
+    my $ip = $_[0];
+    my ( $start_sec, $start_mcsecond ) = gettimeofday();
+    my $response = HTTP::Tiny->new->get("http://$ip");
+    $_ = $response->{content};
+    my ( $end_sec, $end_mcsecond ) = gettimeofday();
+    return ( $end_sec - $start_sec ) +
+      ( $end_mcsecond - $start_mcsecond ) / ( 1000 * 1000 );
+}
+
+sub check_ssh {
+    my $ip = $_[0];
+}
+sub check_icmp {
+    my $host = $_[0];
+    my $p = Net::Ping->new();
+    $p->hires();
+    my ($ret , $duraiton, $ip) = $p->ping($host,5.5);
+    printf "$ip %s ms\n",$duraiton * 1000;
+}
+my @ips = request_github();
 foreach my $ip (@ips) {
-    print "$ip\n";
+    check_icmp($ip);
 }
 
