07844c29d2f7966c6f19fdb9fa48dfce4b34d1b0
licong <licong@qianxin.com>
2020-08-01 16:19:40 +0800

    fix github check

--- src/github_check.pl
+++ src/github_check.pl
@@ -6,37 +6,49 @@
 use warnings;
 use strict;
 
-use LWP::UserAgent;
-use JSON;
 use Time::HiRes qw(gettimeofday);
 use Net::Ping;
+use threads;
+use threads::shared;
+use HTTP::Tiny;
 
-
-# 返回 github 的所有 ip 地址
-sub request_github {
-    my $ua           = LWP::UserAgent->new(Agent => 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36');
-    my $req          = HTTP::Request->new(GET => 'https://api.github.com/meta');
-    $req->header(Accept => "application/vnd.github.v3+json");
-    my $res          = $ua->request($req);
-    unless($res->is_success) {
-        print $res->status_line, "\n";
-   } 
-    my $content      = $res->content;
-    my $json         = JSON->new->allow_nonref;
-    my $response_obj = $json->decode($content);
-
-    my @ips = @{ $response_obj->{git} };
-    push @ips, @{ $response_obj->{web} };
-
-    my @result;
-    foreach my $ip (@ips) {
-        # 原 ip 格式为 "192.30.252.0/22",
-        my ( $ip, $mask ) = split( /\//, $ip );
-        push @result, $ip;
-    }
-    return @result;
-}
-
+my @ips = qw(
+   192.30.252.0
+   185.199.108.0
+   140.82.112.0
+   13.114.40.48
+   52.192.72.89
+   52.69.186.44
+   15.164.81.167
+   52.78.231.108
+   13.234.176.102
+   13.234.210.38
+   13.229.188.59
+   13.250.177.223
+   52.74.223.119
+   13.236.229.21
+   13.237.44.5
+   52.64.108.95
+   18.228.52.138
+   18.228.67.229
+   18.231.5.6
+   18.181.13.223
+   54.238.117.237
+   54.168.17.15
+   3.34.26.58
+   13.125.114.27
+   3.7.2.84
+   3.6.106.81
+   18.140.96.234
+   18.141.90.153
+   18.138.202.180
+   52.63.152.235
+   3.105.147.174
+   3.106.158.203
+   54.233.131.104
+   18.231.104.233
+   18.228.167.86
+);
 sub check_http {
     my $ip = $_[0];
     my ( $start_sec, $start_mcsecond ) = gettimeofday();
@@ -57,8 +69,20 @@ sub check_icmp {
     my ($ret , $duraiton, $ip) = $p->ping($host,5.5);
     printf "%-18s %2.2fs \n",$ip , $duraiton;
 }
-my @ips = request_github();
-foreach my $ip (@ips) {
-    check_icmp($ip);
-}
 
+my $count :shared;
+$count = $#ips;
+foreach my $ip (@ips) {
+    threads->new(sub{
+        check_icmp($_[0]);
+        {
+            lock($count);
+            $count = $count -1
+        }
+    },$ip)->detach();
+}
+while(1) {
+    unless($count) {
+        exit(0);
+    }
+}
