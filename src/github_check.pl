be4fa1d11181a5f9551f8bb04d158b6f4f32e825
hellojukay <licong@qianxin.com>
2020-08-02 22:02:56 +0800

    fix format

--- src/github_check.pl
+++ src/github_check.pl
@@ -26,12 +26,12 @@ sub github_ip{
         my @web_ip;
         my @git_ip;
         my $response = decode_json $content;
-        my @web =  @{$response->{web}};
+        my @web      = @{$response->{web}};
+        my @git      = @{$response->{git}};
         foreach my $ip_with_mask (@web) {
             my ($ip,$mask) = split /\//, $ip_with_mask;
             push @web_ip, $ip;
         }
-        my @git =  @{$response->{git}};
         foreach my $ip_with_mask (@git) {
             my ($ip,$mask) = split /\//, $ip_with_mask;
             push @git_ip, $ip;
@@ -46,9 +46,9 @@ print "waiting https://api.github.com/meta ...";
 sub check_http {
     my $ip = $_[0];
     my ( $start_sec, $start_mcsecond ) = gettimeofday();
-    my $response = HTTP::Tiny->new->get("https://$ip");
-    $_ = $response->{content};
-    my ( $end_sec, $end_mcsecond ) = gettimeofday();
+    my $response                       = HTTP::Tiny->new->get("https://$ip");
+    $_                                 = $response->{content};
+    my ( $end_sec, $end_mcsecond )     = gettimeofday();
     return sprintf("%2.2f",(( $end_sec - $start_sec ) + ( $end_mcsecond - $start_mcsecond ) / ( 1000 * 1000 )));
 }
 
@@ -57,35 +57,31 @@ sub check_http {
 # 返回 2 个值，($ok,$time), $ok 表示是否链接成功，$time 表示链接耗时
 sub check_ssh {
     my $sock = IO::Socket::INET->new(
-        Proto => "tcp",
+        Proto    => "tcp",
         PeerAddr => $_[0],
         PeerPort => 22,
     );
     my ( $ssh_start_sec, $ssh_start_mcsecond ) = gettimeofday();
+    my $len                                    = 1;
     my $buf;
-    my $len = 1;
     if(!defined($sock)){
         return "TIMEOUT";
     }
     $sock->sysread($buf,$len);
     my ($ssh_end_sec, $ssh_end_mcsecond ) = gettimeofday();
-    my $ssh_time = ($ssh_end_sec - $ssh_start_sec) + ($ssh_end_mcsecond - $ssh_start_mcsecond) / (1000*1000);
+    my $ssh_time                          = ($ssh_end_sec - $ssh_start_sec) + ($ssh_end_mcsecond - $ssh_start_mcsecond) / (1000*1000);
     return sprintf("%2.2fs",$ssh_time);
 }
 
-
-my $github = github_ip();
+my $github         = github_ip();
 print("done\n");
-my @web_ip = $github->web;
-my @git_ip = $github->git;
-foreach my $git (@web_ip) {
-    print $git;
-}
+my @web_ip         = @{$github->web};
+my @git_ip         = @{$github->git};
 my $count :shared;
-$count = $#web_ip + $#git_ip;
+$count             = $#web_ip + $#git_ip;
 foreach my $ip (@web_ip) {
     threads->new(sub{
-        my $host = $_[0];
+        my $host          = $_[0];
         my $http_duration = check_http($host);
         printf "%-20s%-10s%-10s\n",$host,$http_duration,"HTTPS";
         {
@@ -96,7 +92,7 @@ foreach my $ip (@web_ip) {
 }
 foreach my $ip (@git_ip) {
     threads->new(sub{
-        my $host = $_[0];
+        my $host         = $_[0];
         my $ssh_duration = check_ssh($host);
         printf "%-20s%-10s%-10s\n",$host,$ssh_duration, "SSH";
         {
